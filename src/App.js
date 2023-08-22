21bc72e36dc9645a83a68f2fb971797f42406a31
Carsten Juul Lund <clu@rts.dk>
2023-08-22 13:18:24 +0200

    add watchpos and navigation

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import PWAPrompt from 'react-ios-pwa-prompt'
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
             <header>
                 <p>This is header</p>
+                <nav>
+                  <Link to="/">Home</Link> &nbsp;&nbsp;
+                  <Link to="/location">Location</Link>
+                </nav>
             </header>
             <main>
                 <Outlet />
--- src/pages/Location.js
+++ src/pages/Location.js
@@ -3,26 +3,43 @@ import { useState, useEffect } from "react";
 const Location = () => {
 
     const [myPos, setMyPos] = useState();
+    const [dynPos, setDynPos] = useState();
 
     useEffect(() => {
         if('geolocation' in navigator) {
-            navigator.geolocation.watchPosition((position) => {
+            navigator.geolocation.getCurrentPosition((position) => {
                 console.log(position)
                 setMyPos(position)
             })
         }
-
     }, []);
+
+
+    const handleDynamicPosition = (position) => {
+        setDynPos(position)
+    }
+    const handleDynamicPositionError = (err) => {
+        console.log(err)
+    }
+
+    navigator.geolocation.watchPosition(handleDynamicPosition, handleDynamicPositionError, {timeout: 5000})
 
     return ( 
         <>
-            <h1>This is Location!</h1>
+            <h1>This is static Location!</h1>
 
             <p>Latitude: {myPos?.coords.latitude}</p>
             <p>Longitude: {myPos?.coords.longitude}</p>
             <p>Altitude: {myPos?.coords.altitude}</p>
             <p>speed: {myPos?.coords.speed}</p>
             <p>Accuracy: {myPos?.coords.accuracy}</p>
+
+            <h1>This is dynamic position</h1>
+                <p>Latitude: {dynPos?.coords.latitude}</p>
+                <p>Longitude: {dynPos?.coords.longitude}</p>
+                <p>Altitude: {dynPos?.coords.altitude}</p>
+                <p>speed: {dynPos?.coords.speed}</p>
+                <p>Accuracy: {dynPos?.coords.accuracy}</p>
         </>
      );
 }
