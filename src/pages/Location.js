d7f771476097eb063344309ee7a5c804885f527a
Carsten Juul Lund <clu@rts.dk>
2023-08-22 12:47:58 +0200

    added geolocation

--- src/pages/Location.js
+++ src/pages/Location.js
@@ -1,9 +1,28 @@
-
+import { useState, useEffect } from "react";
 
 const Location = () => {
+
+    const [myPos, setMyPos] = useState();
+
+    useEffect(() => {
+        if('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                console.log(position)
+                setMyPos(position)
+            })
+        }
+
+    }, []);
+
     return ( 
         <>
             <h1>This is Location!</h1>
+
+            <p>Latitude: {myPos?.coords.latitude}</p>
+            <p>Longitude: {myPos?.coords.longitude}</p>
+            <p>Altitude: {myPos?.coords.altitude}</p>
+            <p>speed: {myPos?.coords.speed}</p>
+            <p>Accuracy: {myPos?.coords.accuracy}</p>
         </>
      );
 }
