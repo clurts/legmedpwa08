bab442a8c3b07e560c1bc12cd0bab9563a55a16b
Carsten Juul Lund <clu@rts.dk>
2023-08-24 10:49:14 +0200

    added iphone support to gyro

--- src/pages/Gyro.js
+++ src/pages/Gyro.js
@@ -1,28 +1,53 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 const Gyro = () => {
     const [deviceAlpha, setDeviceAlpha] = useState();
     const [deviceBeta, setDeviceBeta] = useState();
     const [deviceGamma, setDeviceGamma] = useState();
-
+    const [isIOS, setIsIOS] = useState(false);
 
     useEffect(() => {
-        window.addEventListener("deviceorientation", e => {
-            setDeviceAlpha(e.alpha.toFixed(1))
-            setDeviceBeta(e.beta.toFixed(1))
-            setDeviceGamma(e.gamma.toFixed(1))
-        })
+        if (navigator.userAgent.match(/iPhone/i)) {
+            setIsIOS(true);
+        }
     }, []);
-    return ( 
+
+    useEffect(() => {
+        window.addEventListener("deviceorientation", (e) => {
+            setDeviceAlpha(e.alpha.toFixed(1));
+            setDeviceBeta(e.beta.toFixed(1));
+            setDeviceGamma(e.gamma.toFixed(1));
+        });
+    }, []);
+
+    const enableDeviceOrientation = () => {
+        DeviceOrientationEvent.requestPermission().then((response) => {
+            if (response === "granted") {
+                window.addEventListener("deviceorientation", (e) => {
+                    setDeviceAlpha(e.alpha.toFixed(1));
+                    setDeviceBeta(e.beta.toFixed(1));
+                    setDeviceGamma(e.gamma.toFixed(1));
+                });
+            }
+        });
+    };
+
+    return (
         <>
             <h1>Gyroskop</h1>
-
+            {isIOS && <p>Du er på en iPhone!</p>}
             <h2>Værdier fra gyroskopet:</h2>
             <p>alpha: {deviceAlpha}</p>
             <p>beta: {deviceBeta}</p>
             <p>gamma: {deviceGamma}</p>
+
+            {isIOS && !deviceAlpha ? (
+                <button onClick={enableDeviceOrientation}>
+                    Enable on iPhone
+                </button>
+            ) : null}
         </>
-     );
-}
- 
+    );
+};
+
 export default Gyro;
