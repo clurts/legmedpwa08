92264c2144752e0c5cdaaccc664707baf0ba1ba0
Carsten Juul Lund <clu@rts.dk>
2023-08-24 09:31:13 +0200

    added gyro page

--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import { createBrowserRouter, createRoutesFromElements, Route } from "react-rout
 import App from "./App";
 import Home from "./pages/Home";
 import Location from "./pages/Location";
+import Gyro from "./pages/Gyro";
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />} >
             <Route index element={<Home />} />
             <Route path="/location" element={<Location />} />
+            <Route path="/gyro" element={<Gyro />} />
             
         </Route>
     )
