3fd96cbc1be35aa84a60bfd346f287aa13dc7e43
Carsten Juul Lund <clu@rts.dk>
2023-08-25 09:35:55 +0200

    added mapbox

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { Outlet } from 'react-router-dom';
-import PWAPrompt from 'react-ios-pwa-prompt'
-import { Link } from 'react-router-dom';
-import OneSignal from 'react-onesignal';
+import React, { useEffect } from "react";
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import PWAPrompt from "react-ios-pwa-prompt";
+import { Link } from "react-router-dom";
+import OneSignal from "react-onesignal";
 
 function App() {
-
-  useEffect(() => {
-    OneSignal.init({ appId: process.env.REACT_APP_ONESIGNAL });
-  }, []);
-  return (
-    <>
+    useEffect(() => {
+        OneSignal.init({ appId: process.env.REACT_APP_ONESIGNAL });
+    }, []);
+    return (
+        <>
             <header>
                 <p>This is header</p>
                 <nav>
-                  <Link to="/">Home</Link> &nbsp;&nbsp;
-                  <Link to="/location">Location</Link>
+                    <Link to="/">Home</Link> &nbsp;&nbsp;
+                    <Link to="/location">Location</Link>&nbsp;&nbsp;
+                    <Link to="/gyro">Gyroscope</Link>&nbsp;&nbsp;
+                    <Link to="/map">Map</Link>
                 </nav>
             </header>
             <main>
@@ -27,7 +28,7 @@ function App() {
             </footer>
             <PWAPrompt copyTitle="Føj til hjemmeskærm" />
         </>
-  );
+    );
 }
 
 export default App;
